56c998ba03eeb9c5172935448b15e9bf1d280f41
Adeboye Bamkole <67976121+Bamkole25@users.noreply.github.com>
2024-05-30 17:25:13 +0100

    Add files via upload

---
+++ pir_motion_sensor_sketch.ino
@@ -0,0 +1,19 @@
+int buzzer = A5;
+int inputPin = A4;
+int pirState = LOW;
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(buzzer, OUTPUT);
+ pinMode(inputPin, OUTPUT);
+
+ Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ val = digitalRead(iputPin);
+ if (val == HIGH){
+ digitalWrite(buzzer, HIGH);
+  
+ }
+}
