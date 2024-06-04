422e3aefe7654b05d95f5b30a901c1da2f83673f
Adeboye Bamkole <67976121+Bamkole25@users.noreply.github.com>
2024-06-04 14:41:58 +0100

    Add files via upload

--- pir_motion_sensor_sketch.ino
+++ pir_motion_sensor_sketch.ino
@@ -1,22 +1,25 @@
 int buzzer = A5;
 int inputPin = A4;
 int pirState = LOW;
+
 void setup() {
   // put your setup code here, to run once:
  pinMode(buzzer, OUTPUT);
- pinMode(inputPin, OUTPUT);
+ pinMode(inputPin, INPUT);
 
  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- int val = digitalRead(inputPin);
- if (val == HIGH){
- //digitalWrite(buzzer, HIGH); // for active buzzers
-tone(buzzer, 1000, 2000); // for passive buzzers 
+ pirState = digitalRead(inputPin);
+ Serial.println(pirState);
+ if (pirState == HIGH){
+  digitalWrite(buzzer, HIGH); // for active buzzers
+  //tone(buzzer, 1000, 2000); // for passive buzzers 
  }
  else {
-  noTone(buzzer);
+  digitalWrite(buzzer, LOW); // for active buzzers
+  //noTone(buzzer); // for passive buzzers
  }
 }
